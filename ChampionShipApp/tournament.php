2d4de7bbf288b1499db73f9327e3984ca8ea48cb
blackrabbit910 <ghostfaster910@gmail.com>
2022-04-20 16:29:45 +0400

    Довёл до ума систему сессий

--- ChampionShipApp/tournament.php
+++ ChampionShipApp/tournament.php
@@ -1,5 +1,15 @@
 <?php
+
 require('require.php');
+
+session_start();
+print_r($arrayPairs);
+
+?>
+<pre><?php print_r($challenger3);?></pre>
+<?php
+
+
 
 ?>
 <html>
@@ -7,16 +17,21 @@ require('require.php');
     <head>
         <link rel="stylesheet" href="css/tournament.css">
     </head>
-    <body>
+    <body>        
+        <div class='navPanel'>
+            <a href="../index.php"><div class='navBlock'>Tournament</div></a> 
+            <a href="../championshipapp/scoreTable.php"><div class='navBlock'>SCORE</div></a>
+            <a href="destroy.php"><div class='navBlock'>destroy</div></a>
+        </div>
         <div class='challengers'>
 
-        
+
             <div class='challengerPairs'>
                 
             <button class='buttonChallengers'><img class='imgChallengers' src="../img/challenger/<?= $challenger1->getIdBlock(); ?>.jpg"></button>
 
             <button class='buttonChallengers'><img class='imgChallengers' src="../img/challenger/<?= $challenger2->getIdBlock(); ?>.jpg"></button>
-   
+
             </div>
 
 
@@ -33,9 +48,3 @@ require('require.php');
 
 
 ?>
-
-<pre>
-    <?php
-        print_r($linkDB);
-    ?>
-</pre>
